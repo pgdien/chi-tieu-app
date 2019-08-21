759609d4be5c4aba5199fb5621d345b8cbfd8c5e
pgdien <pgdien2@gmail.com>
2019-08-22 01:26:19 +0700

    1

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve static files....
 app.use(express.static(__dirname + '/dist/chi-tieu-app'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/chi-tieu-app/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/chi-tieu-app/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
